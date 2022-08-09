095e6f9d75b5f08847e6dc44ed50f2577560bd7a
ExternalP <110000600+ExternalP@users.noreply.github.com>
2022-08-09 17:14:40 +0800

    readme

--- FormWiki.cs
+++ FormWiki.cs
@@ -12,11 +12,24 @@ using System.Windows.Forms;
  * Date: 9/8/2022
  * Purpose: AT1 - Project Wiki Prototype */
 /* Case Study – Data Structures Wiki Catalogue
- * 
+As a senior programmer for CITE Managed Services develop a wiki app prototype 
+ for junior programmers cataloguing Data Structures with the fields Name, 
+ Category, Structure & Definition using a Window Forms Application.
+A rudimentary interface design is provided.
+Need to use GitHub version control for project.
+Basic Structure:
+ - ONLY allowed 1 class: FormWIki.cs
+ - Simple global 2D String array to store records: myRecordsArray[11, 3].
+ - Records' 4 String fields: recName, recCategory, recStructure & recDefinition.
+ - Can add/edit/delete records
+ - Search records by Name.
+ - Binary file efinitions.dat to stored the records by create/save/load from it.
+
  */
 /* Functionality 
  * 
  */
+// DONT FORGET to copy to README.md
 namespace AT1_WikiPrototype
 {
     public partial class FormWiki : Form
