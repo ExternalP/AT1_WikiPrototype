4c600790a54fce575ce325745a6a10c7f81de5e2
ExternalP <110000600+ExternalP@users.noreply.github.com>
2022-08-09 11:02:32 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* Name: Corin Little
+ * ID: P453208
+ * Date: 9/8/2022
+ * Purpose: AT1 - Project Wiki Prototype */
+/* Case Study – Data Structures Wiki Catalogue
+ * 
+ */
+/* Functionality 
+ * 
+ */
+namespace AT1_WikiPrototype
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
