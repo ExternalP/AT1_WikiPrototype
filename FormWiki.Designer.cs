fe8b8ce71f922b863aa4cfc1dec17ca7ec763b15
ExternalP <110000600+ExternalP@users.noreply.github.com>
2022-08-16 14:48:49 +0800

    form edit

--- FormWiki.Designer.cs
+++ FormWiki.Designer.cs
@@ -29,12 +29,56 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listViewRecords = new System.Windows.Forms.ListView();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnCategory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listViewRecords
+            // 
+            this.listViewRecords.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnName,
+            this.columnCategory});
+            this.listViewRecords.FullRowSelect = true;
+            this.listViewRecords.GridLines = true;
+            this.listViewRecords.HideSelection = false;
+            this.listViewRecords.Location = new System.Drawing.Point(267, 12);
+            this.listViewRecords.Name = "listViewRecords";
+            this.listViewRecords.Size = new System.Drawing.Size(205, 287);
+            this.listViewRecords.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.listViewRecords, "Select the \'Name\' of record to display details");
+            this.listViewRecords.UseCompatibleStateImageBehavior = false;
+            this.listViewRecords.View = System.Windows.Forms.View.Details;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            this.columnName.Width = 120;
+            // 
+            // columnCategory
+            // 
+            this.columnCategory.Text = "Category";
+            this.columnCategory.Width = 80;
+            // 
+            // FormWiki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 311);
+            this.Controls.Add(this.listViewRecords);
+            this.Name = "FormWiki";
+            this.Text = "Wiki Prototype";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewRecords;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnCategory;
     }
 }
 
